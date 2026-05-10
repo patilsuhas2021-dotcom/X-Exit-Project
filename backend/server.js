c886603aaf324fee6f7ba132faa228f728fb5030
patilsuhas2021-dotcom <patilsuhas2021@gmail.com>
2026-05-10 13:49:27 +0530

    Comment out server start logic in development
    Port updated

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,13 @@
 require("dotenv").config();
 const app = require("./src/app");
 
-if (process.env.NODE_ENV !== 'production') {
+//if (process.env.NODE_ENV !== 'production') {
+
   const PORT = process.env.PORT || 5000;
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
   });
-}
+
+//}
 
 module.exports = app;
